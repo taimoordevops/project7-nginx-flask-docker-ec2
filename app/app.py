9e9f0bf24576138583e88d12b1286d1386858fe1
Taimoor DevOps <yourgithub@email.com>
2025-08-13 17:25:36 -0800

    Update Flask app message to include 'via Nginx'

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return '✅ Hello from Flask Docker App!'
+@app.route("/")
+def hello():
+    return "✅ Hello from Flask Docker App via Nginx!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
